a2c183ce5bc530e0c35fd9dfc778ab24b6e40178
Andrey Tyshchenko <ggoldenfeniks@gmail.com>
2023-04-05 02:07:09 +1000

    Copy files to example folder on install

--- install.ps1
+++ install.ps1
@@ -6,12 +6,14 @@ cp source/fefu_base.dtx $CACHE_DIR
 cp source/fefu.dtx $CACHE_DIR
 cp source/fefu_presentation.dtx $CACHE_DIR
 cd $CACHE_DIR
+
 latex fefu.ins
 pdflatex fefu_base.dtx
 pdflatex fefu_style_default.dtx
 pdflatex fefu_style_imct.dtx
 pdflatex fefu.dtx
 pdflatex fefu_presentation.dtx
+
 cd ../
 mkdir -Force cls
 cp $CACHE_DIR/fefu_base.sty cls
@@ -19,10 +21,18 @@ cp $CACHE_DIR/fefu_style_default.sty cls
 cp $CACHE_DIR/fefu_style_imct.sty cls
 cp $CACHE_DIR/fefu.cls cls
 cp $CACHE_DIR/fefu_presentation.cls cls
+
 mkdir -Force doc
 cp $CACHE_DIR/fefu_base.pdf doc
 cp $CACHE_DIR/fefu_style_default.pdf doc
 cp $CACHE_DIR/fefu_style_imct.pdf doc
 cp $CACHE_DIR/fefu.pdf doc
 cp $CACHE_DIR/fefu_presentation.pdf doc
+
+cp $CACHE_DIR/fefu_base.sty example
+cp $CACHE_DIR/fefu_style_default.sty example
+cp $CACHE_DIR/fefu_style_imct.sty example
+cp $CACHE_DIR/fefu.cls example
+cp $CACHE_DIR/fefu_presentation.cls example
+
 rm -r $CACHE_DIR
